1c9563de98fdd4c9a82b358c44bce2d38426c4ea
wahyubudies.dct <wahyu.setyawan@digitalcenter.id>
2024-03-15 10:25:49 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+export { default as Chat } from "./components/Chat.jsx";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
 );
